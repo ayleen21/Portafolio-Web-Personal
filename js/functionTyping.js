b0ced9afc3103125044c3cfcec5d1fcee5050246
ayleen21 <ayleenmurillo77@gmail.com>
2022-06-16 10:11:58 -0500

    Se actualizo la parte de los conocimientos

---
+++ js/functionTyping.js
@@ -0,0 +1,16 @@
+// // ------Variables------
+// var i,text;
+
+// i= 0;
+
+// text="Hola, soy Ayleen...";
+
+// function typing(){
+//     if(i<text.length){
+//         document.getElementById("text").innerHTML += text.charAt(i);
+//         i++;
+//         setTimeout(typing,50);
+//     }
+// }
+
+// typing();
